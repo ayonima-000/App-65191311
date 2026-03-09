859f8a715bda798293b324a7821a121c4be56904
ayonima-000 <ayonimaayonima3@gmail.com>
2026-03-09 10:23:00 +0700

    replace config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    turbopack: {
-      // Allow the custom webpack config from next-pwa
-    }
-  }
-};
+  // Tidak perlu experimental.turbopack lagi
+  // Cukup kosongkan atau tambah opsi lain jika perlu
+}
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig
